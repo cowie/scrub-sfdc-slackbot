ca9c53050c7595603b282e1f915d200d839b33d2
Chris De Gour <cdegour@cdegour-ltm2.internal.salesforce.com>
2016-03-29 09:56:37 -0700

    hrm

--- env.js
+++ env.js
@@ -1,3 +1,3 @@
-process.env['clientId'] = process.env.CLIENTID || '27868865686.30288538628';
-process.env['clientSecret'] = process.env.CLIENTSECRET || 'd73ed97b1add952082c5bdb32fb2753f';
-process.env['port'] = process.env.PORT || '4144';
+process.env['clientId'] = process.env.CLIENTID ;//|| '27868865686.30288538628';
+process.env['clientSecret'] = process.env.CLIENTSECRET; //|| 'd73ed97b1add952082c5bdb32fb2753f';
+process.env['port'] = process.env.PORT; //|| '4144';
