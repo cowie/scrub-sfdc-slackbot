a8315875f644586417bee82446049110c7028484
Chris De Gour <cdegour@cdegour-ltm2.internal.salesforce.com>
2016-04-08 14:13:59 -0700

    Repushing for SFDC Demo

--- scratchBot.js
+++ scratchBot.js
@@ -25,7 +25,7 @@ This is a sample Slack Button application that adds a bot to one or many slack t
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
 
 /* Uses the slack button feature to offer a real time bot to multiple teams */
-var Botkit = require('botkit');
+var Botkit = require('botkit'); 
 var Redis_Store = require('./redis_storage.js');
 var redis_url = process.env.REDIS_URL || "redis://127.0.0.1:6379";
 var redis_store = new Redis_Store({url: redis_url});
