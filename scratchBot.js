c9d6e4343be0a38abad8e786ae19871c924109d3
Chris De Gour <cdegour@cdegour-ltm2.internal.salesforce.com>
2016-04-11 14:27:26 -0700

    fixing our attempt to make hanul awesome

--- scratchBot.js
+++ scratchBot.js
@@ -171,7 +171,7 @@ controller.setupWebserver(process.env.port,function(err,webserver) {
 //USECASE 2: API TO CREATE CHANNELS AS NECESSARY
 //USECASE 3: HEAR Message to update, update accurate record in SFDC
 controller.hears('update (.*) to (.*)', earsMentionOnly, function(bot, message){
-  bot.reply(message, "Yo, so first, matches0 is:" + message.matches[0] + ", next matches1 is:" + message.matches[1]+ ", finally matches2 is:" + message.matches[2]);
+  bot.reply(message, "Yo, so first, matches0 is:" + message.match[0] + ", next matches1 is:" + message.match[1]+ ", finally matches2 is:" + message.match[2]);
 });
 //USECASE 4: HEAR message w attachment, send attachment to SFDC.
 
